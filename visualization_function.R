9d17877500bf96c1023a68dce4f11bb99f2c2561
sisitzky <90338095+sisitzky@users.noreply.github.com>
2021-10-09 16:09:37 -0400

    html

--- visualization_function.R
+++ visualization_function.R
@@ -6,8 +6,8 @@ gapminder.model <- lm(Mortality ~ Vaccination_Rate, data = mortality.vacc)
 summary(gapminder.model)
 
 ## creating plot_gg function which will display ggplot output
-plot_gg <- function(tibble, variable1, variable2, .fitted = ".fitted") {
-  augment(tibble) %>%
+plot_gg <- function(model, variable1, variable2, .fitted = ".fitted") {
+  augment(model) %>%
     ggplot() +
     geom_point(aes_string(x = variable1, y = variable2), color = "red") +
     geom_line(aes_string(x = variable1, y = .fitted)) +
@@ -16,3 +16,18 @@ plot_gg <- function(tibble, variable1, variable2, .fitted = ".fitted") {
 
 ## running plot_gg function
 plot_gg(gapminder.model, "Vaccination_Rate", "Mortality") + labs(x = "Vaccination Rate", y = "Mortality")
+
+## filtering data for second model of recent data
+
+filtered.data <- filter(mortality.vacc, Year >= 2000)
+
+## generating linear model for the relationship between the two variables with filtered data
+
+gapminder.model2 <- lm(Mortality ~ Vaccination_Rate, data = filtered.data)
+summary(gapminder.model2)
+
+## running plot_gg function with filtered data
+plot_gg(gapminder.model2, "Vaccination_Rate", "Mortality") + labs(x = "Vaccination Rate", y = "Mortality")
+
+
+
